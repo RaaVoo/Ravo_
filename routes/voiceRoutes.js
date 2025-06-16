0a7151300edf43fd23c0c310c3b8a193b9cfcd20
WJE <jongeun0010@duksung.ac.kr>
2025-06-16 23:48:14 +0900

    보고서 저장

--- routes/voiceRoutes.js
+++ routes/voiceRoutes.js
@@ -6,5 +6,4 @@ router.post('/reports', voiceController.createVoiceReport);
 router.get('/reports-list', voiceController.getVoiceReportList);
 router.get('/:voice_no', voiceController.getVoiceReportById);
 router.delete('/reports-list/:voice_no', voiceController.deleteVoiceReport);
-
 module.exports = router;
