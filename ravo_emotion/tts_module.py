37e9d6725647bf42045fbfd49702108de182b8e1
summer <etnsumm@gmail.com>
2025-06-17 04:32:09 +0900

    fix: 백엔드 AI 연결

--- ravo_emotion/tts_module.py
+++ ravo_emotion/tts_module.py
@@ -1,7 +1,10 @@
 from gtts import gTTS
 import os
+import time
 
-def speak_text(text, filename="response.mp3"):
+def speak_text(text):
+    filename = f"response_{int(time.time())}.mp3"
+    
     tts = gTTS(text=text, lang="ko")
     tts.save(filename)
     os.system(f"start {filename}")
